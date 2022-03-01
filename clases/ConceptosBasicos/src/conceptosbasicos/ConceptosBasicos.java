c6a8575a034c80ed253a11f487a14e3956cf4903
YahirLimas <yahirlimasfuentes@gmail.com>
2022-03-01 12:53:00 -0600

    Sesion 1 de marzo, automovil

--- clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptosbasicos;
 
+import ico.fes.Automovil;
+import ico.fes.Silla;
+import java.awt.Color;
+
 /**
  *
  * @author YAHIR LIMAS
@@ -16,6 +20,23 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola de nuevo");
+        Silla s1 = new Silla();
+        System.out.println(s1);//Por defecto se invoca toString()
+        System.out.println(s1.toString());
+        
+        Automovil miBocho = new Automovil();
+        System.out.println(miBocho);
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.blue);
+        System.out.println(miBocho);
+        
+        Automovil miMustang = new Automovil("Ford", "Mustang", 2010, Color.yellow);
+        System.out.println(miMustang);
+        
+        Automovil miAkura = new Automovil("Akura", "NSX", 2013, Color.gray);
+        System.out.println(miAkura);
     }
     
 }
