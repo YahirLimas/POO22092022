7a9fb69a09329ba87f79353557e2b6d440077ef0
YahirLimas <yahirlimasfuentes@gmail.com>
2022-04-28 13:00:09 -0500

    Clase 28 de Abril, Excepciones

---
+++ clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaBasico2209Parte2.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javabasico2209parte2;
+
+import ico.fes.Aritmetica;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author YAHIR LIMAS
+ */
+public class JavaBasico2209Parte2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        Excepciones
+        */
+        ArrayList<String> nombres = new ArrayList<String>();
+        nombres.add("Alma");
+        nombres.add("Bartolo");
+        nombres.add("Carlos");
+        nombres.add("Diana");
+        nombres.add("Ernesto");
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+        System.out.println("----------");
+        Scanner teclado = new Scanner(System.in);
+        int seleccion = 0;
+        
+        String elNombre = "";
+        try{
+            seleccion = teclado.nextInt();
+            elNombre = nombres.get(seleccion);
+        }catch (InputMismatchException ie){
+            System.out.println("Debes teclear valores numéricos");
+            elNombre = nombres.get(0);
+        }catch (IndexOutOfBoundsException ioe){
+            System.out.println("Error, debe ser entre 0 y 4");
+            elNombre = nombres.get(0);
+        }catch (Exception e){
+            System.out.println("Genérica");
+            elNombre = nombres.get(1);
+        }finally{
+            System.out.println(elNombre);
+            elNombre = null;
+            //Limpieza
+        }
+        
+        //System.out.println(nombres.get(seleccion));
+        
+        System.out.println("Continúa el programa...");
+        
+        Aritmetica cal = new Aritmetica(2, 0);
+        System.out.println(cal.getA() + " + " + cal.getB() + " = " + cal.sumar());
+        
+        try {
+            System.out.println(cal.getA() + " / " + cal.getB() + " = " + cal.dividir());
+        } catch (Exception e) {
+            System.out.println("Error de aritmetica");
+        }
+        System.out.println("Fin del programa.");
+    }   
+}
