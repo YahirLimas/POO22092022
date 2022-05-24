13b4f96c00dab34fb1d8a288a2277e792f4027c1
YahirLimas <yahirlimasfuentes@gmail.com>
2022-05-24 13:02:08 -0500

    Clase 24 de Mayo, Bases de datos

---
+++ clases/JavaBasico2209Parte2/src/ico/fes/db/TestSQLite.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.db;
+
+import java.sql.*;//No es recomendable, pero ahorramos tiempo ahorita
+
+/**
+ *
+ * @author YAHIR LIMAS
+ */
+public class TestSQLite {
+    public static void main(String[] args) {
+        Connection conexion = null;
+        try {
+            String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "/poo2022.db";
+            System.out.println(System.getProperty("user.dir") + "/poo2022.db");
+            conexion = DriverManager.getConnection(url);
+            System.out.println("Conexión establecida...");
+            Statement stm = conexion.createStatement();
+            //stm.execute("CREATE TABLE prueba1(nombre VARCHAR(10), telefono VARCHAR(15));");
+            stm.execute(" INSERT INTO prueba1 VALUES('Diana','4455677')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- clases/JavaBasico2209Parte2/src/ico/fes/modelo/ModeloPersonaCombo.java
+++ clases/JavaBasico2209Parte2/src/ico/fes/modelo/ModeloPersonaCombo.java
@@ -74,4 +74,7 @@ public class ModeloPersonaCombo implements ComboBoxModel<Persona>{
         datos.add(new Persona("Jesus", 33));
         datos.add(new Persona("Diana", 22));
     }
+    public void agregarPersona (Persona p){
+        datos.add(p);
+    }
 }
